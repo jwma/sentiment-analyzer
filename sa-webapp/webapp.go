f1ee45f108575e9b5e6bf24641e0dd6ba62ca56d
MJ <mjw@majiawei.com>
2019-08-12 22:36:46 +0800

    feat(sa-webapp): 实现 webapp  功能
    - 使用 go mod
    - 使用 Gin

---
+++ sa-webapp/webapp.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type AnalyseSentimentResponse struct {
+	Code int        `json:"code"`
+	Data *Sentiment `json:"data"`
+}
+
+type AnalyseSentimentData struct {
+	Sentence string `json:"sentence"`
+}
+
+type Sentiment struct {
+	Sentence   string  `json:"sentence"`
+	Sentiments float32 `json:"sentiments"`
+}
+
+func sendAnalyseRequest(reqData *AnalyseSentimentData) ([]byte, error) {
+	reqBody, _ := json.Marshal(reqData)
+	url := os.Getenv("API_HOST") + "/analyse/sentiment"
+	fmt.Println(url)
+	resp, _ := http.Post(url, "application/json", bytes.NewBuffer(reqBody))
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func analyseSentiment(reqData *AnalyseSentimentData) *AnalyseSentimentResponse {
+	resp, _ := sendAnalyseRequest(reqData)
+	rs := &AnalyseSentimentResponse{
+		Data: &Sentiment{},
+	}
+	_ = json.Unmarshal(resp, rs)
+	return rs
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/analyse", func(c *gin.Context) {
+		reqData := &AnalyseSentimentData{}
+		_ = c.BindJSON(reqData)
+		resp := analyseSentiment(reqData)
+		c.JSON(http.StatusOK, resp.Data)
+	})
+	err := router.Run()
+	if err != nil {
+		panic("启动失败...")
+	}
+}
