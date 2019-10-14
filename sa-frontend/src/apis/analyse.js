a87b333f750358369142e9892f395d38ede03777
MJ <mjw@majiawei.com>
2019-10-14 16:04:40 +0800

    fix(api): 修复 API 地址

--- sa-frontend/src/apis/analyse.js
+++ sa-frontend/src/apis/analyse.js
@@ -3,6 +3,6 @@ import Vue from 'vue'
 const API_HOST = process.env.VUE_APP_API_HOST
 
 export function getResult(sentence) {
-    return Vue.prototype.$http.post(API_HOST, JSON.stringify({sentence}))
+    return Vue.prototype.$http.post(API_HOST + '/analyse', JSON.stringify({sentence}))
 }
 
